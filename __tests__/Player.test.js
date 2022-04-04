3dd2cb8212bc483132f116b334f9426fe03547c6
AD <archied5150@yahoo.com>
2022-04-04 18:47:28 -0500

    added all player methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,5 +1,6 @@
 //import the player constructor
 const { test, expect } = require('@jest/globals');
+const { TestResult } = require('@jest/types');
 const expectExport = require('expect');
 const Player = require('../lib/Player');
 //import the potion constructor
@@ -44,3 +45,65 @@ test('gets inventory form player or returns false', () => {
     //now expect an empty inventory 
     expect(player.getInventory()).toEqual(false);
 });
+
+test("gets player's health value", () => {
+    const player = new Player('Dave');
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+//run test to see if it correctly tells if player is alive or dead
+test('checks if player is alive or not', () => {
+    const player = new Player('Dave');
+
+    //check if player is alive
+    expect(player.isAlive()).toBeTruthy();
+    //set player health to 0
+    player.health = 0;
+    //ckeck if player is dead
+    expect(player.isAlive()).toBeFalsy();
+});
+
+//test if the reduction of player health works properly
+test("subtracts from player's health", () => {
+    const player = new Player('dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth - 5);
+
+    player.reduceHealth(99999);
+
+    expect(player.health).toBe(0);
+});
+
+//verifies that a player's attack value is within range
+test("gets player's attack value", () => {
+    const player = new Player('Dave');
+    player.strength = 10;
+
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+test('adds a potion to the inventory', () => {
+    const player = new Player('Dave');
+    //capture the current inventory size
+    const oldCount = player.inventory.length;
+    //add a potion to the inventory
+    player.addPotion(new Potion());
+    //compare the new and old inventory size
+    expect(player.inventory.length).toBeGreaterThan(oldCount);
+});
+
+test('uses a potion from inventory', ()=>{
+    const player = new Player('Dave');
+    //add 3 new potions to the user;s inventory
+    player.inventory = [new Potion(), new Potion(), new Potion()];
+    //record the current inventory size
+    const oldCount = player.inventory.length;
+    //have the player use a potion
+    player.usePotion(1);
+    expect(player.inventory.length).toBeLessThan(oldCount);
+})
